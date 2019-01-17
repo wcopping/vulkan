9d0fc45fea571ebd99ae660ea08b5ed05466d3b5
wyatt <wcopping@asu.edu>
2019-01-17 14:11:36 -0700

    finished graphics pipeline basics with render pass implementation

--- getting-started/main.cpp
+++ getting-started/main.cpp
@@ -146,6 +146,7 @@ private:
   VkFormat swapchain_image_format;
   VkExtent2D swapchain_extent;
   std::vector<VkImageView> swapchain_image_views;
+  VkRenderPass render_pass;
   VkPipelineLayout pipeline_layout;
 
 
@@ -318,7 +319,45 @@ private:
     create_logical_device();
     create_swapchain();
     create_image_views();
+    create_render_pass();
     create_graphics_pipeline();
+  }
+
+
+  void create_render_pass()
+  {
+    VkAttachmentDescription color_attachment = {};
+    color_attachment.format  = swapchain_image_format;
+    color_attachment.samples = VK_SAMPLE_COUNT_1_BIT;
+    /* applies to color and depth data */
+    color_attachment.loadOp  = VK_ATTACHMENT_LOAD_OP_CLEAR;
+    color_attachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+    /* applies to stencil data */
+    color_attachment.stencilLoadOp  = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+    color_attachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+    /* pixel format of textures and framebuffers can change based on what we're
+       doing */
+    color_attachment.initialLayout  = VK_IMAGE_LAYOUT_UNDEFINED;
+    color_attachment.finalLayout    = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+
+    VkAttachmentReference color_attachment_ref = {};
+    color_attachment_ref.attachment = 0;
+    color_attachment_ref.layout     = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+
+    VkSubpassDescription subpass = {};
+    subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
+    subpass.colorAttachmentCount = 1;
+    subpass.pColorAttachments    = &color_attachment_ref;
+
+    VkRenderPassCreateInfo render_pass_ci = {};
+    render_pass_ci.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
+    render_pass_ci.attachmentCount = 1;
+    render_pass_ci.pAttachments = &color_attachment;
+    render_pass_ci.subpassCount = 1;
+    render_pass_ci.pSubpasses   = &subpass;
+
+    if (vkCreateRenderPass(device, &render_pass_ci, nullptr, &render_pass) != VK_SUCCESS)
+      throw std::runtime_error("Failed to create render pass!");
   }
 
 
@@ -701,6 +740,7 @@ private:
   void cleanup()
   {
     vkDestroyPipelineLayout(device, pipeline_layout, nullptr);
+    vkDestroyRenderPass(device, render_pass, nullptr);
     for (auto image_view : swapchain_image_views) {
       vkDestroyImageView(device, image_view, nullptr);
     }
